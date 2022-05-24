24ae367a57993a618f96f4743e7c012353c78981
boo <booblegum42@gmail.com>
2022-05-24 06:44:51 -0700

    base template complete. Add migrations

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from MainApp import views
 
 urlpatterns = [
-    path('', views.index_page),
+    path('', views.index_page, name="home"),
     path('snippets/add', views.add_snippet_page, name="snippets-add"),
     path('snippets/list', views.snippets_page, name="snippets-list"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
